055f927d96f9ebb043c5c9cad217c7e8d4cbad55
Xavier Raynaud <xavier.raynaud@sintef.no>
2024-03-22 19:30:45 +0100

    more in graph doc

--- GraphDocumentation/scripts/runScript.m
+++ GraphDocumentation/scripts/runScript.m
@@ -5,25 +5,18 @@ set(0, 'defaultaxesfontsize', 15);
 % model = ReactionModel2();
 % model = ThermalModel();
 % model = ReactionThermalModel();
+model = UncoupledReactionThermalModel();
 % model = ConcentrationModel();
 % model = ConcentrationReactionModel();
-model = ConcentrationReactionThermalModel();
+% model = ConcentrationReactionThermalModel();
 cgp = model.cgp;
 cgt = model.cgt;
-% h = cgp.plot();
-h = cgp.plotModelGraph();
+h = cgp.plot();
+% h = cgp.plotModelGraph();
 set(h, 'nodefontsize', 14);
 % set(h, 'nodefontsize', 9);
 set(h, 'linewidth', 3);
 set(h, 'arrowsize', 20);
-% t = text(h.XData, h.YData          , ...
-%          h.NodeLabel                        , ...  %the first 2 arguments provide the position and we can give individual X and Ypositions of each node
-%          'Units'              , 'inches', ...
-%          'VerticalAlignment'  , 'top'       , ...                
-%          'HorizontalAlignment', 'left'      , ... % alignment with the node
-%          'Parent', gca      , ... % alignment with the node
-%          'FontSize'           , 14);
-% h.NodeLabel = {};
 
 dosave = true;
 savedir = '../img';
@@ -33,7 +26,8 @@ if dosave
     % filename = 'reacmodelgraph2.png';
     % filename = 'tempgraph.png';
     % filename = 'concreacgraph.png';
-    filename = 'tempconcreacgraphmodel.png';
+    % filename = 'tempconcreacgraphmodel.png';
+    filename = 'uncoupledreactemp.png';
     filename = fullfile(savedir, filename);
     saveas(gcf, filename)
 end
